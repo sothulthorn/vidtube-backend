0a29b1b3ba32143af3601440d38fab506901131b
sothulthorn <thornsothul@gmail.com>
2024-12-09 21:48:28 +0900

    connect mongodb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello from vidtube backend api");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8001;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Error: ", error);
+  });
